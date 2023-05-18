02aeaa06eea8014cc090896a7dbf25d874c099b6
Christoph Ender <Christoph.Ender@Spellbreaker.org>
2023-05-18 11:12:39 +0200

    Precompiled hyphenation patterns.

---
+++ src/hyph_patterns/pattern_setup.c
@@ -0,0 +1,357 @@
+
+/* pattern_setup.c
+ *
+ * This file is part of fizmo.
+ *
+ * Copyright (c) 2010-2023 Christoph Ender.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Doing TeX hyphenation; see The TeXbook, Appendix H
+
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "../tools/z_ucs.h"
+#include "../tools/filesys.h"
+#include "../tools/list.h"
+
+#include "hyph_pattern_tools.h"
+
+
+static int divide(z_ucs **patterns, int start, int end) {
+  int i=start, j=end;
+  z_ucs *pattern;
+  z_ucs *pivot = patterns[end];
+
+  do {
+    while ( (i < end) && (cmp_pattern(patterns[i], pivot) >= 0 ) ) {
+      i++;
+    }
+
+    while ( (j >= start) && (cmp_pattern(patterns[j], pivot) <= 0 ) ) {
+      j--;
+    }
+
+    if (i < j) {
+      pattern = patterns[i];
+      patterns[i] = patterns[j];
+      patterns[j] = pattern;
+    }
+  }
+  while (i < j);
+
+  if (cmp_pattern(patterns[i], pivot) < 0) {
+    pattern = patterns[i];
+    patterns[i] = patterns[end];
+    patterns[end] = pattern;
+  }
+
+  return i;
+}
+
+
+static void sort_patterndata(z_ucs **patterns, int start, int end) {
+  int div;
+
+  if (start < end) {
+    div = divide(patterns, start, end);
+    sort_patterndata(patterns, start, div - 1);
+    sort_patterndata(patterns, div + 1, end);
+  }
+}
+
+
+static int load_patterns(char *filename, FILE *output_file, char *locale_code) {
+  z_file *patternfile;
+  z_ucs input;
+  z_ucs *pattern_input_buffer = NULL;
+  size_t pattern_input_buffer_size = 0;
+  size_t pattern_input_buffer_index = 0;
+  list *input_patterns = create_list();
+  z_ucs **parsed_patterns;
+  size_t parsed_patterns_size;
+  size_t parsed_patterns_index;
+  size_t *pattern_indices = NULL;
+  size_t pattern_indices_index = 0;
+  size_t pattern_indices_size = 0;
+  size_t pattern_data_index = 0;
+  size_t output_index;
+  size_t i;
+
+  patternfile = fsi->openfile(filename, FILETYPE_DATA, FILEACCESS_READ);
+
+  do {
+    input = parse_utf8_char_from_file(patternfile);
+    if (input == (z_ucs)'%') {
+      // Found comment, skip over it.
+      do {
+        input = parse_utf8_char_from_file(patternfile);
+      }
+      while ( (input != Z_UCS_NEWLINE) && (input != UEOF) );
+    }
+    else if (input != UEOF) {
+      pattern_input_buffer_index = 0;
+      while ( (input != Z_UCS_NEWLINE) && (input != UEOF) ) {
+        // ensure there's at least space for two z_ucs since we've
+        // also got to terminate the string at the end.
+        if (pattern_input_buffer_index + 1 >= pattern_input_buffer_size) {
+          pattern_input_buffer_size += 128;
+          if ((pattern_input_buffer = realloc(
+                  pattern_input_buffer, pattern_input_buffer_size * sizeof(z_ucs)))
+              == NULL) {
+            fputs("Couldn't allocate memory.", stderr);
+            exit(-1);
+          }
+        }
+        pattern_input_buffer[pattern_input_buffer_index++] = input;
+        input = parse_utf8_char_from_file(patternfile);
+      }
+      // end of regular pattern string, terminate string in-memory.
+      pattern_input_buffer[pattern_input_buffer_index++] = 0;
+      add_list_element(input_patterns, z_ucs_dup(pattern_input_buffer));
+    }
+  }
+  while (input != UEOF);
+  fsi->closefile(patternfile);
+
+  pattern_input_buffer_index = 0;
+  pattern_input_buffer_size = 0;
+  free(pattern_input_buffer);
+  pattern_input_buffer = NULL;
+
+  parsed_patterns_size = get_list_size(input_patterns);
+  parsed_patterns = (z_ucs**)delete_list_and_get_ptrs(input_patterns);
+
+  sort_patterndata(parsed_patterns, 0, parsed_patterns_size - 1);
+
+  pattern_indices_index = 0;
+  pattern_indices_size = parsed_patterns_size;
+  pattern_indices = malloc(sizeof(size_t) * pattern_indices_size);
+
+  fprintf(output_file, "static z_ucs pattern_data_%s[] = {\n  ", locale_code);
+  parsed_patterns_index = 0;
+  pattern_data_index = 0;
+  output_index = 0;
+  while (parsed_patterns_index < parsed_patterns_size) {
+    pattern_indices[pattern_indices_index] = pattern_data_index;
+    i = 0;
+    do {
+      if (output_index == 6) {
+        output_index  = 0;
+        fprintf(output_file, ",\n  ");
+      }
+      else if (output_index  != 0) {
+        fprintf(output_file, ", ");
+      }
+      fprintf(output_file, "0x%08x", parsed_patterns[parsed_patterns_index][i]);
+      pattern_data_index++;
+      output_index++;
+      i++;
+    }
+    while (parsed_patterns[parsed_patterns_index][i] != 0);
+    parsed_patterns_index++;
+    pattern_indices_index++;
+  }
+  fprintf(output_file, "\n};\n\n");
+
+  fprintf(output_file, "static z_ucs *patterns_%s[] = {\n  ", locale_code);
+  pattern_indices_index = 0;
+  i = 0;
+  while (pattern_indices_index < pattern_indices_size) {
+    if (i == 3) {
+      i = 0;
+      fprintf(output_file, ",\n  ");
+    }
+    else if (i != 0) {
+      fprintf(output_file, ", ");
+    }
+    fprintf(output_file, "pattern_data_%s+%*zu", locale_code, 5, pattern_indices[pattern_indices_index]);
+    i++;
+    pattern_indices_index++;
+  }
+  fprintf(output_file, "\n};\n\n");
+
+  free(pattern_indices);
+
+  return pattern_indices_size;
+}
+
+
+void write_disclaimer_to_file(FILE *output_file, char *filename) {
+  fprintf(output_file, "/*\n"
+      " * %s\n"
+      " *\n"
+      " * This file is part of fizmo.\n"
+      " *\n"
+      " * Copyright (c) 2010-2023 Christoph Ender.\n"
+      " * All rights reserved.\n"
+      " *\n"
+      " * Redistribution and use in source and binary forms, with or without\n"
+      " * modification, are permitted provided that the following conditions\n"
+      " * are met:\n"
+      " * 1. Redistributions of source code must retain the above copyright\n"
+      " *    notice, this list of conditions and the following disclaimer.\n"
+      " * 2. Redistributions in binary form must reproduce the above copyright\n"
+      " *    notice, this list of conditions and the following disclaimer in the\n"
+      " *    documentation and/or other materials provided with the distribution.\n"
+      " * 3. Neither the name of the copyright holder nor the names of its\n"
+      " *    contributors may be used to endorse or promote products derived\n"
+      " *    from this software without specific prior written permission.\n"
+      " * \n"
+      " * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS\n"
+      " * IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,\n"
+      " * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n"
+      " * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR\n"
+      " * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,\n"
+      " * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,\n"
+      " * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR\n"
+      " * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF\n"
+      " * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING\n"
+      " * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n"
+      " * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+      " */\n", filename);
+}
+
+
+int main(int argc, char *argv[]) {
+ struct dirent *dp;
+ DIR *dfd;
+ FILE *output_file;
+ char *locale_code;
+ list *locale_code_list;
+ int nof_locale_codes;
+ char **locale_codes;
+ int *nof_patterns;
+ char input_filename[10];
+ int i, j;
+
+ if ((dfd = opendir(".")) == NULL) {
+   fputs("Can't open dir", stderr);
+   return 0;
+ }
+
+ locale_code_list = create_list();
+ while ((dp = readdir(dfd)) != NULL)
+ {
+   struct stat stbuf ;
+   if( stat(dp->d_name,&stbuf ) == -1 )
+   {
+     printf("Unable to stat file: %s\n",dp->d_name) ;
+     continue ;
+   }
+
+   if ( ( stbuf.st_mode & S_IFMT ) == S_IFDIR ) {
+     continue;
+   }
+   else {
+     if (strlen(dp->d_name) == 9) { // strlen("xx_XX.txt") == 9
+       locale_code= malloc(6);
+       strncpy(locale_code, dp->d_name, 5);
+       locale_code[5] = 0;
+       add_list_element(locale_code_list, locale_code);
+     }
+   }
+ }
+ nof_locale_codes = get_list_size(locale_code_list);
+ nof_patterns = (int*)malloc(sizeof(int) * nof_locale_codes);
+ locale_codes = (char**)delete_list_and_get_ptrs(locale_code_list);
+
+ output_file = fopen("hyph_patterns.c", "w");
+ write_disclaimer_to_file(output_file, "hyph_patterns.c");
+ fprintf(output_file,
+     "\n"
+     "#include <stdlib.h>\n"
+     "\n"
+     "#include \"../tools/z_ucs.h\"\n"
+     "#include \"../tools/stringmap.h\"\n"
+     "#include \"hyph_pattern_tools.h\"\n"
+     "\n"
+     "static stringmap *pattern_map = NULL;\n"
+     "\n");
+
+ i=0;
+ while (i < nof_locale_codes) {
+   locale_code = locale_codes[i];
+   sprintf(input_filename, "%s.txt", locale_code);
+   printf("Processing \"%s\".\n", input_filename);
+   nof_patterns[i] = load_patterns(input_filename, output_file, locale_code);
+   i++;
+ }
+
+ fprintf(output_file,
+     "\n"
+     "void init_patterns() {\n"
+     "  pattern_map = create_stringmap();\n\n");
+
+ i=0;
+ while (i < nof_locale_codes) {
+   locale_code = locale_codes[i];
+   fprintf(output_file, "  hyph_patterns *hyph_patterns_%s = malloc(sizeof(hyph_patterns));\n",
+       locale_code);
+   fprintf(output_file, "  hyph_patterns_%s->patterns = patterns_%s;\n",
+       locale_code, locale_code);
+   fprintf(output_file, "  hyph_patterns_%s->number_of_patterns = %d;\n\n",
+       locale_code, nof_patterns[i]);
+   i++;
+ }
+
+ i=0;
+ while (i < nof_locale_codes) {
+   locale_code = locale_codes[i];
+   fprintf(output_file, "  z_ucs locale_code_%s[] = {\n    ", locale_code);
+   for (j=0; j<5; j++) {
+     fprintf(output_file, "(z_ucs)'%c', ", locale_code[j]);
+   }
+   fprintf(output_file, "(z_ucs)0 };\n");
+   i++;
+ }
+ fputs("\n", output_file);
+
+ i=0;
+ while (i < nof_locale_codes) {
+   locale_code = locale_codes[i];
+   fprintf(output_file,
+       "  add_stringmap_element(pattern_map, locale_code_%s, hyph_patterns_%s);\n",
+       locale_code, locale_code);
+   i++;
+ }
+ fprintf(output_file, "}\n\n");
+
+ fclose(output_file);
+ free(nof_patterns);
+ free(locale_codes);
+  
+  exit(0);
+}
+
--- src/interpreter/hyphenation.c
+++ src/interpreter/hyphenation.c
@@ -45,7 +45,9 @@
 #include "../tools/list.h"
 #include "../tools/i18n.h"
 #include "../tools/filesys.h"
+#include "../tools/stringmap.h"
 #include "../locales/libfizmo_locales.h"
+#include "../hyph_patterns/hyph_patterns.h"
 #include "config.h"
 #include "fizmo.h"
 #include "hyphenation.h"
@@ -55,337 +57,27 @@ static z_ucs *last_pattern_locale = NULL;
 static z_ucs *pattern_data;
 static z_ucs **patterns;
 static int nof_patterns = 0;
-static z_ucs *search_path = NULL;
-//static z_ucs *subword_buffer = NULL;
-//static int subword_buffer_size = 0;
-//static z_ucs *word_buf = NULL;
-//static int word_buf_len = 0;
 
-// From tools/i18n.c:
-extern char default_search_path[];
-
-
-
-static z_ucs input_char(z_file *in)
-{
-  z_ucs input;
-
-  if ((input = parse_utf8_char_from_file(in)) == UEOF)
-  {
-    TRACE_LOG("Premature end of file.\n");
-    i18n_translate_and_exit(
-        libfizmo_module_name,
-        i18n_libfizmo_ERROR_WHILE_READING_FILE_P0S,
-        -1,
-        "<hyphenation>");
-  }
-
-  return (z_ucs)input;
-}
-
-
-
-// Returns > 0 in case s1 > s2, 0 if equal and < 0 otherwise.
-static int cmp_pattern(z_ucs *s1, z_ucs *s2)
-{
-  while ( (*s1 != 0) || (*s2 != 0) )
-  {
-    if ( (*s1 != 0) && (isdigit(*s1) != 0) )
-      s1++;
-
-    if ( (*s2 != 0) && (isdigit(*s2) != 0) )
-      s2++;
-
-    if (*s1 != *s2)
-      return *s2 - *s1;
-
-    s1++;
-    s2++;
-  }
-
-  return 0;
-}
-
-
-static int divide(int start, int end)
-{
-  int i=start, j=end;
-  z_ucs *pattern;
-  z_ucs *pivot = patterns[end];
-
-  do
-  {
-    while ( (i < end) && (cmp_pattern(patterns[i], pivot) >= 0 ) )
-    {
-      //TRACE_LOG("i: %d\n", i);
-      i++;
-    }
-
-    while ( (j >= start) && (cmp_pattern(patterns[j], pivot) <= 0 ) )
-    {
-      //TRACE_LOG("j: %d\n", j);
-      j--;
-    }
-
-    if (i < j)
-    {
-      pattern = patterns[i];
-      patterns[i] = patterns[j];
-      patterns[j] = pattern;
-    }
-  }
-  while (i < j);
-
-  if (cmp_pattern(patterns[i], pivot) < 0)
-  {
-    pattern = patterns[i];
-    patterns[i] = patterns[end];
-    patterns[end] = pattern;
-  }
-
-  return i;
-}
-
-
-static void sort_patterndata(int start, int end)
-{
-  int div;
-
-  if (start < end)
-  {
-    div = divide(start, end);
-    sort_patterndata(start, div - 1);
-    sort_patterndata(div + 1, end);
-  }
-}
 
 
 static int load_patterns()
 {
-  list *lines;
-  char *path_as_latin1;
-  char *filename = "patterns.txt", *testfilename;
-  z_ucs *colon_index, *current_locale;
-  size_t bufsize = 0, len, filename_len, locale_len;
-  z_ucs *zucs_buf = NULL, *filename_as_zucs;
-  z_file *patternfile = NULL;
-  size_t nof_zucs_chars;
-  z_ucs *linestart;
-  z_ucs input;
-  int in_char;
-  z_ucs *data;
-  size_t nof_comments;
-#ifdef ENABLE_TRACING
-  int i;
-#endif
-
-  if (search_path == NULL)
-  {
-    if ((path_as_latin1 = get_configuration_value("i18n-search-path")) == NULL)
-    {
-      TRACE_LOG("Using default search path.\n");
-      path_as_latin1 = default_search_path;
-    }
-
-    if ((search_path=dup_latin1_string_to_zucs_string(path_as_latin1)) == NULL)
-      return -2;
-  }
+  z_ucs *current_locale;
+  hyph_patterns *pattern_data;
 
   current_locale = get_current_locale_name();
   if (last_pattern_locale != NULL)
     free(last_pattern_locale);
   last_pattern_locale = z_ucs_dup(current_locale);
-  locale_len = z_ucs_len(current_locale);
 
-  TRACE_LOG("pattern path: \"");
-  TRACE_LOG_Z_UCS(search_path);
-  TRACE_LOG("\".\n");
+  pattern_data = (hyph_patterns*)get_stringmap_value(
+      pattern_map, current_locale);
 
-  filename_len = strlen(filename);
-  filename_as_zucs = fizmo_malloc(sizeof(z_ucs) * strlen(filename) + 1);
-  utf8_string_to_zucs_string(filename_as_zucs, filename, filename_len + 1);
-
-  while (*search_path != 0)
+  if (pattern_data != NULL)
   {
-    colon_index = z_ucs_chr(search_path, Z_UCS_COLON);
-
-    len
-      = colon_index == NULL
-      ? z_ucs_len(search_path)
-      : (size_t)(colon_index - search_path);
-
-    TRACE_LOG("len: %ld\n", (long)len);
-
-    if (len > 0)
-    {
-      if (bufsize < len + filename_len + locale_len + 3)
-      {
-        TRACE_LOG("realloc buf for %ld bytes.\n",
-            (long)sizeof(z_ucs) * (len + filename_len + locale_len + 3));
-
-        // open-resource:
-        if ((zucs_buf = realloc(zucs_buf,
-                sizeof(z_ucs) * (len + filename_len + locale_len + 3))) == NULL)
-        {
-          // exit-point:
-          TRACE_LOG("realloc() returned NULL.\n");
-          free(filename_as_zucs);
-          free(zucs_buf);
-          return -5;
-        }
-
-        bufsize = len + filename_len + locale_len + 3;
-      }
-
-      z_ucs_ncpy(zucs_buf, search_path, len);
-      zucs_buf[len] = Z_UCS_SLASH;
-      z_ucs_cpy(zucs_buf + len + 1, current_locale);
-      *(zucs_buf + len + locale_len + 1) = Z_UCS_SLASH;
-      z_ucs_cpy(zucs_buf + len + locale_len + 2, filename_as_zucs);
-
-      if ((testfilename = dup_zucs_string_to_utf8_string(zucs_buf)) == NULL)
-      {
-        free(zucs_buf);
-        free(filename_as_zucs);
-        return -3;
-      }
-
-      TRACE_LOG("Trying pattern file: \"%s\".\n", testfilename);
-
-      patternfile = fsi->openfile(testfilename, FILETYPE_DATA, FILEACCESS_READ);
-      free(testfilename);
-
-      if (patternfile != NULL)
-        break;
-    }
-
-    search_path += len + (colon_index != NULL ? 1 : 0);
-  }
-
-  free(zucs_buf);
-  free(filename_as_zucs);
-
-  if (patternfile != NULL)
-  {
-    TRACE_LOG("File found.\n");
-
-    // We'll now measure the filesize in order to be able to allocate a
-    // single block of memory before reading data. This is necessary, since
-    // we're storing pattern indexes while reading and a realloc might
-    // invalidate these.
-
-    nof_zucs_chars = 0;
-    for(;;)
-    {
-      // Parse line.
-      input = parse_utf8_char_from_file(patternfile);
-      if (input == UEOF)
-        break;
-
-      if (input == (z_ucs)'%')
-      {
-        do
-        {
-          input = parse_utf8_char_from_file(patternfile);
-        }
-        while (input != Z_UCS_NEWLINE);
-      }
-      else
-      {
-        nof_zucs_chars++;
-        while (input != Z_UCS_NEWLINE)
-        {
-          nof_zucs_chars++;
-          input = parse_utf8_char_from_file(patternfile);
-        }
-      }
-    }
-
-    if (fsi->setfilepos(patternfile, 0, SEEK_SET) == -1)
-    {
-      // exit-point:
-      TRACE_LOG("setfilepos() returned -1.\n");
-      fsi->closefile(patternfile);
-      return -6;
-    }
-
-    TRACE_LOG("Allocating space for %ld z_ucs chars.\n", nof_zucs_chars);
-
-    // open-resource:
-    if ((data = malloc(nof_zucs_chars * sizeof(z_ucs))) == NULL)
-    {
-      // exit-point:
-      TRACE_LOG("malloc(%ld) returned NULL.\n", nof_zucs_chars * sizeof(z_ucs));
-      fsi->closefile(patternfile);
-      return -7;
-    }
-    pattern_data = data;
-
-    nof_comments = 0;
-    lines = create_list();
-    //printf("new list created: %p\n", lines);
-
-    in_char = fsi->readchar(patternfile);
-    while (in_char != EOF)
-    {
-      if (in_char == '%')
-      {
-        TRACE_LOG("Start comment.\n");
-        do
-        {
-          input = input_char(patternfile);
-        }
-        while (input != Z_UCS_NEWLINE);
-        nof_comments++;
-      }
-      else
-      {
-        // Found a new line.
-        fsi->unreadchar(in_char, patternfile);
-
-        linestart = data;
-
-        TRACE_LOG("Start pattern.\n");
-        for (;;)
-        {
-          input = input_char(patternfile);
-
-          if (input == Z_UCS_NEWLINE)
-          {
-            *data ++ = 0;
-            TRACE_LOG("New line at %p.\n", linestart);
-            add_list_element(lines, (void*)linestart);
-            break;
-          }
-          else
-          {
-            // Here we've found some "normal" output.
-            *data++ = (z_ucs)input;
-          }
-        }
-
-        //messages_processed++;
-      }
-
-      in_char = fsi->readchar(patternfile);
-    }
-    fsi->closefile(patternfile);
-    nof_patterns = get_list_size(lines);
-    patterns = (z_ucs**)delete_list_and_get_ptrs(lines);
-    TRACE_LOG("Read %d patterns, %ld comments.\n", nof_patterns, nof_comments);
-
-    sort_patterndata(0, nof_patterns - 1);
-
-#ifdef ENABLE_TRACING
-    TRACE_LOG("sorted:\n");
-
-    for (i=0; i<nof_patterns; i++)
-    {
-      TRACE_LOG_Z_UCS(patterns[i]);
-      TRACE_LOG("\n");
-    }
-#endif
-
+    nof_patterns = pattern_data->number_of_patterns;
+    patterns = pattern_data->patterns;
+    TRACE_LOG("Read %d patterns.\n", nof_patterns);
     return 0;
   }
   else
@@ -394,14 +86,6 @@ static int load_patterns()
     return -1;
   }
 }
-
-
-/*
-static int factorial(int n)
-{
-  return n == 1 ? 1: n * factorial(n - 1);
-}
-*/
 
 
 static z_ucs *get_pattern(z_ucs *subword)
@@ -416,20 +100,13 @@ static z_ucs *get_pattern(z_ucs *subword)
 
   while (top - bottom >= 0)
   {
-    //TRACE_LOG("start: bottom: %d, top: %d.\n", bottom, top);
     index = bottom + ( (top - bottom) / 2 );
-
-    //TRACE_LOG("Compare (%d): \"", index);
-    //TRACE_LOG_Z_UCS(patterns[index]);
-    //TRACE_LOG("\".\n");
 
     if ((cmp = cmp_pattern(subword, patterns[index])) == 0)
       return patterns[index];
     else if (cmp > 0)
       top = index - 1;
     else bottom = index + 1;
-
-    //TRACE_LOG("end : bottom: %d, top: %d.\n", bottom, top);
   }
 
   return NULL;
@@ -597,24 +274,6 @@ void free_hyphenation_memory(void)
   {
     free(last_pattern_locale);
     last_pattern_locale = NULL;
-  }
-
-  if (pattern_data != NULL)
-  {
-    free(pattern_data);
-    pattern_data = NULL;
-  }
-
-  if (patterns != NULL)
-  {
-    free(patterns);
-    patterns = NULL;
-  }
-
-  if (search_path == NULL)
-  {
-    free(search_path);
-    search_path = NULL;
   }
 }
 
